garry-rogov/android_frameworks_av

libcameraservice: Add support for miui camera mode
 * devices like ginkgo and some xiaomi sdm660 use miui camera mode in camera
   hal to activate certain functions in camera hal, these are enabled when
   vendor.camera.miui.apk is set to 1 based on sys.camera.miui.apk value

 * if this prop is set by default gcam crashes, so we must do it dynamically

 * xiaomi does this in stock libcameraservice but unfortunately we don't
   have stock android 12 to use prebuilt lib

[garry-rogov 2024-03-31]
 * updated for android 14 QPR2

Signed-off-by: garry-rogov <garry.rogov@gmail.com>

services/camera/libcameraservice/CameraService.cpp
@@ -38,6 +38,7 @@
 #include <aidl/AidlCameraService.h>
 #include <android-base/macros.h>
 #include <android-base/parseint.h>
+#include <android-base/properties.h>
 #include <android/permission/PermissionChecker.h>
 #include <binder/ActivityManager.h>
 #include <binder/AppOpsManager.h>
@@ -107,6 +108,7 @@ namespace android {
 using namespace camera3;
 using namespace camera3::SessionConfigurationUtils;
 
+using base::SetProperty;
 using binder::Status;
 using frameworks::cameraservice::service::V2_0::implementation::HidlCameraService;
 using frameworks::cameraservice::service::implementation::AidlCameraService;
@@ -4210,6 +4212,15 @@ status_t CameraService::BasicClient::startCameraOps() {
     }
 
     mOpsActive = true;
+
+    // Configure miui camera mode
+    if (strcmp(mClientPackageName.c_str(), "com.android.camera") == 0) {
+        SetProperty("sys.camera.miui.apk", "1");
+        ALOGI("Enabling miui camera mode");
+    } else {
+        SetProperty("sys.camera.miui.apk", "0");
+        ALOGI("Disabling miui camera mode");
+    }
 
     // Transition device availability listeners from PRESENT -> NOT_AVAILABLE
     sCameraService->updateStatus(StatusInternal::NOT_AVAILABLE, mCameraIdStr);

HK-ginkgo/android_frameworks_av-hzn

Change-Id: Idc3bc7fe8a3814ccf85d8c9601e9e930aba27e89
Signed-off-by: Hirokixd <86230878+Wrdn28@users.noreply.github.com>

services/camera/libcameraservice/CameraService.cpp
@@ -40,6 +40,7 @@
 #include <android-base/parseint.h>
 #include <android_companion_virtualdevice_flags.h>
 #include <android/companion/virtualnative/IVirtualDeviceManagerNative.h>
+#include <android-base/properties.h>
 #include <binder/ActivityManager.h>
 #include <binder/AppOpsManager.h>
 #include <binder/IPCThreadState.h>
@@ -126,6 +127,7 @@ namespace android {
 using namespace camera3;
 using namespace camera3::SessionConfigurationUtils;
 
+using base::SetProperty;
 using binder::Status;
 using companion::virtualnative::IVirtualDeviceManagerNative;
 using frameworks::cameraservice::service::V2_0::implementation::HidlCameraService;
@@ -4425,6 +4427,15 @@ status_t CameraService::BasicClient::notifyCameraOpening() {
     }
 
     mCameraOpen = true;
+
+    // Configure miui camera mode
+    if (strcmp(sCurrPackageName.c_str(), "com.android.camera") == 0) {
+        SetProperty("sys.camera.miui.apk", "1");
+        ALOGI("Enabling miui camera mode");
+    } else {
+        SetProperty("sys.camera.miui.apk", "0");
+        ALOGI("Disabling miui camera mode");
+    }
 
     // Transition device availability listeners from PRESENT -> NOT_AVAILABLE
     sCameraService->updateStatus(StatusInternal::NOT_AVAILABLE, mCameraIdStr);